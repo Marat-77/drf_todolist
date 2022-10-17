88497d5bb9b11151e3f301693f61f99d14931581
zmd <maratz375@gmail.com>
2022-10-17 16:00:19 +0500

    lesson_4 Edited views UserModelViewSet.

--- users/views.py
+++ users/views.py
@@ -1,10 +1,27 @@
-from rest_framework.viewsets import ModelViewSet
+# from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, \
+    UpdateModelMixin, DestroyModelMixin
 from .models import User
 from .serializers import UserModelSerializer
 
 # Create your views here.
 
 
-class UserModelViewSet(ModelViewSet):
+# class UserModelViewSet(ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserModelSerializer
+
+class UserModelViewSet(ListModelMixin,
+                       RetrieveModelMixin,
+                       UpdateModelMixin,
+                       # DestroyModelMixin,
+                       GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+
+
+# ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+# GenericViewSet
+
+# ListModelMixin,CreateAPIView, DestroyModelMixin,RetrieveAPIView,UpdateAPIView,GenericViewSet
