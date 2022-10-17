84b7c2d912fd8a93d7d636e26f028c9c5a6251e0
zmd <maratz375@gmail.com>
2022-10-17 16:42:26 +0500

    lesson_4 Added filters: ProjectFilter (by contains) and TodoFilter (DateTime).

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,6 +3,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from .models import Project, Todo
 from .serializers import ProjectHyperlinkedModelSerializer
 from .serializers import TodoHyperlinkedModelSerializer
+from .filters import ProjectFilter, TodoFilter
 
 
 # Create your views here.
@@ -17,9 +18,14 @@ class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectHyperlinkedModelSerializer
     pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoHyperlinkedModelSerializer
     pagination_class = ToDoLimitOffsetPagination
+    filterset_class = TodoFilter
+    # filterset_class = TodoProjectFilter
+    # filterset_class = [TodoFilter, TodoProjectFilter]
+    # TodoProjectFilter
