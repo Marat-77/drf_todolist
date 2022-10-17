71140a5eb9127610782a067a50f456d82fc70323
zmd <maratz375@gmail.com>
2022-10-17 16:06:24 +0500

    lesson_4 Added Limit page to pagination for Project and Todo (todoapp).

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,15 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 from .models import Project, Todo
 from .serializers import ProjectHyperlinkedModelSerializer
 from .serializers import TodoHyperlinkedModelSerializer
 
 
 # Create your views here.
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectHyperlinkedModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoHyperlinkedModelSerializer
+    pagination_class = ToDoLimitOffsetPagination
