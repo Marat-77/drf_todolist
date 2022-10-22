37a019beb6061b806e2b1aa7e731e8baef384410
zmd <maratz375@gmail.com>
2022-10-22 11:25:22 +0500

    lesson_6 Add permissions and authentications. Created groups and users for API

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # Register your models here.
 
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email',)}),
+    )
+    list_display = ('id', 'username', 'custom_group', 'email',)
+    list_display_links = ('username',)
+    ordering = ['id']
 
-admin.site.register(User)
+    def custom_group(self, obj):
+        return ','.join(
+            [group.name for group in obj.groups.all()]
+        ) if obj.groups.count() else ''
+
+
+
+# admin.site.register(User)
+
+
+# from django.contrib import admin
+# from users.models import CustomUser
+# from django.contrib.auth.admin import UserAdmin
+#
+#
+# @admin.register(CustomUser)
+# class CustomUserAdmin(UserAdmin):
+#     add_fieldsets = UserAdmin.add_fieldsets + (
+#         (None, {'fields': ('email',)}),
+#     )
+#     list_display = ('id', 'username', 'custom_group', 'email',)
+#     list_display_links = ('username',)
+#     ordering = ['id']
+#
+#     def custom_group(self, obj):
+#         return ','.join([g.name for g in obj.groups.all()]) if obj.groups.count() else ''
--- users/models.py
+++ users/models.py
@@ -1,11 +1,38 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 
 # Create your models here.
 
+# class User(AbstractBaseUser):
+#     id = models.AutoField(primary_key=True, unique=True)
+#     username = models.CharField(max_length=100, unique=True, blank=False)
+#     first_name = models.CharField(max_length=155)
+#     last_name = models.CharField(max_length=155)
+#     email = models.EmailField(max_length=100, unique=True)
+#     USERNAME_FIELD = 'username'
+
 
 class User(AbstractUser):
-    username = models.CharField(max_length=100, unique=True, blank=False)
-    first_name = models.CharField(max_length=155)
-    last_name = models.CharField(max_length=155)
+    # username = models.CharField(max_length=100, unique=True, blank=False)
+    # first_name = models.CharField(max_length=155)
+    # last_name = models.CharField(max_length=155)
     email = models.EmailField(max_length=150, unique=True, blank=False)
+
+
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+# from django.utils.translation import gettext_lazy as _
+#
+#
+# class CustomUser(AbstractUser):
+#     email = models.EmailField(
+#         _('email address'),
+#         unique=True,
+#     )
+#
+#     class Meta:
+#         verbose_name = _('user')
+#         verbose_name_plural = _('users')
+#
+#     def __str__(self):
+#         return f"{self.pk} {self.username}"
